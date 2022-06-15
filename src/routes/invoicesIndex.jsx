0f6b75d8e41a4db9b75dc52b816b49ffe9f069fa
yangming4711 <13552045220@163.comcd>
2022-06-15 10:08:43 +0800

    1.useSearchParams

---
+++ src/routes/invoicesIndex.jsx
@@ -0,0 +1,38 @@
+/*
+ * @Author: yangming
+ * @Date: 2022-06-14 22:22:55
+ * @Description: 
+ */
+import { NavLink, Outlet } from "react-router-dom";
+import { getInvoices } from "../data";
+
+export default function Invoices() {
+  let invoices = getInvoices();
+  return (
+    <div style={{ display: "flex" }}>
+      <nav
+        style={{
+          borderRight: "solid 1px",
+          padding: "1rem",
+        }}
+      >
+        {invoices.map((invoice) => (
+          <NavLink
+            style={({ isActive }) => {
+              return {
+                display: "block",
+                margin: "1rem 0",
+                color: isActive ? "red" : "",
+              };
+            }}
+            to={`/invoices/${invoice.number}`}
+            key={invoice.number}
+          >
+            {invoice.name}
+          </NavLink>
+        ))}
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
